cec0ceecdd93dd72340d2845ba53b9b6d314be09
Nate <nmbook@gmail.com>
2012-09-29 00:05:33 -0400

    - Now handles the given nick properly. - IRCState data can now be changed during an event, because it is now part of the return value of events. - All IRC messages are now parsed with the splitIRCLn function, which always returns a list of the form [prefix, command, arg1, arg2, ..., argrest]. prefix is always present and is "" when no prefix was provided. - Updated the way messages are constructed to use a data type (MkMessage Source Command [CommandArgs]) - As a test, all PING messages are responded to with PONG.

--- hircbot.hs
+++ hircbot.hs
@@ -11,6 +11,29 @@ type Pass = String
 type Nick = String
 type ChannelName = String
 type Flags = Int
+type Source = String
+type Command = String
+type CommandArgs = [String]
+
+data Message = MkMessage Source Command CommandArgs
+getMSource :: Message -> Source
+getMSource (MkMessage s _ _) = s
+getMCommand :: Message -> Command
+getMCommand (MkMessage _ c _) = c
+getMArgs :: Message -> CommandArgs
+getMArgs (MkMessage _ _ a) = a
+
+instance Show Message where
+    show (MkMessage "" cmd args) = cmd ++ showArgs args
+    show (MkMessage prefix cmd args) = ":" ++ prefix ++ " " ++ cmd ++ showArgs args
+
+--instance Show CommandArgs where
+showArgs :: CommandArgs -> String
+--showArgs [""] = ""
+showArgs (arg1:argrest) = if (length $ words arg1) == 1
+                          then " " ++ arg1 ++ showArgs argrest
+                          else " :" ++ arg1
+showArgs _ = ""
 
 -- "enum" used to choose what type of event handle_event is handling
 data EventType = OnConnect | OnReadLine | OnPipe | OnClose deriving (Enum)
@@ -21,6 +44,19 @@ getASNick :: AuthState -> Nick
 getASNick (MkAuthState nick _) = nick
 getASPass :: AuthState -> Pass
 getASPass (MkAuthState _ pass) = pass
+
+-- the connection settings, this stuff will be added to
+data IRCState = MkIRCState Handle AuthState
+getIRCSocket :: IRCState -> Handle
+getIRCSocket (MkIRCState sock _) = sock
+getIRCAuth :: IRCState -> AuthState
+getIRCAuth (MkIRCState _ as) = as
+
+data EventReturn = MkReturn [Message] IRCState
+getRetMsgs :: EventReturn -> [Message]
+getRetMsgs (MkReturn m _) = m
+getRetState :: EventReturn -> IRCState
+getRetState (MkReturn _ s) = s
 
 -- user data type
 data User = MkUser Nick Flags
@@ -36,52 +72,81 @@ getChName (MkChannel name _) = name
 getChUsers :: Channel -> [User]
 getChUsers (MkChannel _ users) = users
 
--- the connection settings, this stuff will be added to
-data IRCState = MkIRCState Handle AuthState
-getIRCSocket :: IRCState -> Handle
-getIRCSocket (MkIRCState sock _) = sock
-getIRCAuth :: IRCState -> AuthState
-getIRCAuth (MkIRCState _ as) = as
-
 -- main: impure, takes cli and connects to given server
 main = withSocketsDo $
-        do 
-           args <- getArgs
-           let host:[nick:_] = args in do
-               sock <- connectTo host $ PortNumber 6667
-               let state = MkIRCState sock (MkAuthState "HIRCTEST" "") in do
-                   send_responses state $ handle_event OnConnect state ""
-                   recv_loop state
+    do 
+       args <- getArgs
+       let host:(nick:_) = args in do
+           sock <- connectTo host $ PortNumber 6667
+           let state = MkIRCState sock (MkAuthState nick "") in
+               let ret = handle_event OnConnect state "" in do
+                   send_responses (getRetState ret) (getRetMsgs ret)
+                   recv_loop $ getRetState ret
 
 -- impure, handles receiving from socket
 recv_loop :: IRCState -> IO ()
-recv_loop state = do
-                     line <- hGetLine $ getIRCSocket state
-                     putStrLn $ "Recv: " ++ line
-                     send_responses state $ handle_event OnReadLine state line
-                     recv_loop state
-                     return ()
+recv_loop state =
+    do
+       line <- hGetLine $ getIRCSocket state
+       putStrLn $ "Recv: " ++ line
+       let ret = handle_event OnReadLine state line in do
+           send_responses (getRetState ret) (getRetMsgs ret)
+           recv_loop $ getRetState ret
 
 -- impure, sends each given response to socket
-send_responses :: IRCState -> [String] -> IO ()
+send_responses :: IRCState -> [Message] -> IO ()
 send_responses state (a:b)    = let sock = getIRCSocket state in do
-                                    hPutStrLn sock a
-                                    putStrLn $ "Send: " ++ a
+                                    hPutStrLn sock $ show a
+                                    putStrLn $ "Send: " ++ (show a)
                                     send_responses state b
 send_responses state []       = return ()
 
 
 -- pure, takes an event and returns a list of responses to the server,
 -- this is the main functionality of this client
-handle_event :: EventType -> IRCState -> String -> [String]
+handle_event :: EventType -> IRCState -> String -> EventReturn
 -- on connect, send NICK
 handle_event OnConnect state _ =
+    MkReturn 
     [
-    "NICK " ++ getASNick (getIRCAuth state),
-    "USER " ++ getASNick (getIRCAuth state) ++ " 0 * :Nate",
-    "JOIN #rochack"
+    MkMessage "" "NICK" [getASNick $ getIRCAuth state],
+    MkMessage "" "USER" [getASNick $ getIRCAuth state, "0", "*", ":hircbot"],
+    MkMessage "" "JOIN" ["##rochack"]
     ]
+    state
+
+-- handle server command
+handle_event OnReadLine state line =
+    handle_line state $ splitIRCLn line
 
 -- unhandled events return no output
-handle_event _ _ _ = []
+handle_event _ state _ = MkReturn [] state
+
+-- split something of format [:prefix] <command> [arg0 arg1 ... argn] [:argrest]
+-- into [prefix, command, arg0, arg1, ... argn, argrest]
+splitIRCLn :: String -> [String]
+splitIRCLn "" = [""]
+splitIRCLn line =
+    -- does the line start with : (if so prefix is present)
+    let chr1:_ = line in
+        if chr1 == ':'
+        then let (prefix, command) = break (== ' ') line in
+            -- use prefix and parse rest of list
+            (drop 1 prefix) : (drop 1 . splitIRCLn . drop 1 $ command)
+        else let (command, args) = break (== ' ') line in
+            if length args <= 1
+            then "" : [command]
+            else if (head . drop 1 $ args) == ':'
+                then let rest = drop 2 args in
+                    "" : [command, rest]
+                else "" : (command : (drop 1 . splitIRCLn . drop 1 $ args))
+
+-- handle server line
+handle_line :: IRCState -> [String] -> EventReturn
+handle_line state (source:(command:args)) =
+    handle_line_2 state source command args
+
+handle_line_2 :: IRCState -> String -> String -> [String] -> EventReturn
+handle_line_2 state _ "PING" args = MkReturn [MkMessage "" "PONG" []] state
+handle_line_2 state _ _ _ = MkReturn [] state
 
