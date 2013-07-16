7a380541ce9859efbee7a393e6494f3a5d16ef84
Nate Book <nmbook@gmail.com>
2013-07-16 00:33:09 -0400

    Partial changeset to change computers. I don't remember details as the changes were done a while ago.

--- HircState.hs
+++ HircState.hs
@@ -6,9 +6,11 @@ module HircState
     User(..), readUser, UserPrefix(..), Channel(..), readChannel, ChannelPrefix(..),
     HircConfig(..), HircCState(..), HircState(..)) where
 
+import List (find)
+import Data.List (isPrefixOf)
 import System.IO (IO)
 import GHC.IO.Handle (Handle)
-import Network (PortNumber)
+import Network (PortNumber, PortID)
 
 data Word = Word String
 
@@ -51,10 +53,10 @@ instance Show UserPrefix where
     show IsServer = "*"
     show _ = ""
 
-data User = User { nick :: Nick
-                 , username :: Username
-                 , host :: Host
-                 , prefix :: UserPrefix }
+data User = User { userNick :: Nick
+                 , userName :: Username
+                 , userHost :: Host
+                 , userPrefix :: UserPrefix }
 
 readUser :: String -> User
 readUser name@(prefix:rest)
@@ -95,25 +97,125 @@ instance Show Channel where
     show (Channel n t u p) = (show p) ++ n
 
 -- HircConfig: Configuration as loaded from CL args and config file
-data HircConfig = HircConfig { cfgHost :: Host
-                             , cfgPort :: PortNumber
-                             , cfgNick :: Nick
-                             , cfgUser :: Username
-                             , cfgPass :: Pass
-                             , cfgRealName :: RealName
-                             , cfgChans :: ChannelName }
+--data HircConfig = HircConfig { cfgHost :: Host
+--                             , cfgPort :: PortNumber
+--                             , cfgNick :: Nick
+--                             , cfgUser :: Username
+--                             , cfgPass :: Pass
+--                             , cfgRealName :: RealName
+--                             , cfgChans :: ChannelName }
 
 -- HircCState: current (active) state data
 -- the channels here are what the bot is currently in
-data HircCState = HircCState { curSocket :: Handle
-                             , curNick :: Nick
-                             , curChannels :: [Channel] }
-                | HircCStateDisc
+--data HircCState = HircCState { curSocket :: Handle
+--                             , curNick :: Nick
+--                             , curChannels :: [Channel] }
+--                | HircCStateDisc
 
 -- HircState: the persistant state of Everything.
 -- two sub-parts: config and current
 -- config: what was loaded from the CL args and config file; when the bot connects it will use this data to initialize the current settings
 -- current: what is currently true; when things happen, this will change to reflect those changes
-data HircState = HircState { stateCfg :: HircConfig
-                           , stateCur :: HircCState }
+--data HircState = HircState { stateCfg :: HircConfig
+--                           , stateCur :: HircCState }
+
+
+
+
+
+-- A HircState is a map of state data:
+-- group, key, value
+
+-- group name
+data HircStateKeyGroup = Cfg | Cur | Usr | Chn deriving (Show,Read,Eq,Enum)
+
+-- key name
+type HircStateKey = String
+
+-- values: typed values
+data HircStateValue = HostValue { host :: NetworkHost }
+                    | NameValue { name :: String }
+                    | PassValue { pass :: String }
+                    | ChannelNamesValue { channelNames :: [String] }
+                    | HandlesValue { handles :: [NamedHandle] }
+                    | ChannelsValue { channels :: [Channel] }
+                    | ChannelValue { c :: Channel }
+                    | UsersValue { users :: [User] }
+                    | UserValue { u :: User }
+                    | NullValue
+
+-- actual data:
+--   cfg.Server = NetworkHost
+--   cfg.Nick = Name
+--   cfg.Username = Name
+--   cfg.Realname = Realname
+--   cfg.Pass = Pass
+--   cfg.Channels = ChannelNames
+--
+--   cur.Handles = Handles
+--   cur.Channels = Channels
+--   cur.Users = Users
+--   cur.Nick = Name
+--   cur.Username = Name
+--   cur.Realname = Realname
+--   cur.Pass = Pass
+--
+--   usr.[name] = User
+--   chn.[name] = Channel
+type HircStateRecord = (HircStateKeyGroup, HircStateKey, HircStateValue)
+
+newtype HircState = HircState [HircStateRecord]
+
+sGrp :: HircStateRecord -> HircStateKeyGroup
+sGrp (grp,_,_) = grp
+
+sKey :: HircStateRecord -> HircStateKey
+sKey (_,key,_) = key
+
+sVal :: HircStateRecord -> HircStateValue
+sVal (_,_,val) = val
+
+sGet :: HircState -> HircStateKeyGroup -> HircStateKey -> Maybe HircStateValue
+sGet = fmap sVal sGetRecord
+
+sMatchRecord :: HircStateKey -> HircStateValue -> HircStateRecord -> Bool
+sMatchRecord grp key (rgrp, rkey, _) = rgrp == grp && rkey == key
+
+sGetRecord :: HircState -> HircStateKeyGroup -> HircStateKey -> Maybe HircStateRecord
+sGetRecord state grp key = find (sMatchRecord grp key) state
+
+sPut :: HircState -> HircStateKeyGroup -> HircStateKey -> HircStateValue -> HircState
+sPut state grp key val = sPutRecord state (grp, key, val)
+
+sPutRecord :: HircState -> HircStateRecord -> HircState
+sPutRecord state rec = [rec] ++ (sDelRecord rec)
+
+sDelRecord :: HircState -> HircStateRecord -> HircState
+sDelRecord state (grp, key, _) = filter (not . sMatchRecord $ grp key) state
+
+sDel :: HircState -> HircStateKeyGroup -> HircStateKey -> HircState
+sDel state grp key = sDelRecord state $ sGetRecord grp key
+
+-- types available:
+
+-- NamedHandle, allows you to name sockets stored in the state
+-- NamedHandle with the name "irc:<server>" is the irc server given
+-- NamedHandle with the name "log:<date>" is the current log file
+-- NamedHandle with the name 
+-- other NamedHandle can be created for other purposes (such as HTTP requests, etc), must 
+data NamedHandle = NamedHandle { hName :: String, h :: Handle }
+
+handleByName :: [NamedHandle] -> String -> Maybe Handle
+handleByName list name = fmap h $ find ((== name) . hName) list
+
+handleByNameStartsWith :: [NamedHandle] -> String -> [NamedHandle]
+handleByNameStartsWith list nameSW = filter ((isPrefixOf nameSW) . hName) list
+
+putHandle :: [NamedHandle] -> String -> Handle -> [NamedHandle]
+putHandle list name handle = [NamedHandle name handle] ++ (filter ((/= name) . hName) list)
+
+-- NetworkHost, a server and port
+data NetworkHost = NetworkHost { domain :: String, port :: PortID }
+
+
 
